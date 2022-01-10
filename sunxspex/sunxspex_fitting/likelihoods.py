fixed hessian bug that stops size 0 step sizes, add variables and model functions protect globals now, renew_model method has been added to fitting code to allow complicated models to be re-added once loaded back in to a different session without overwriting the parameter table etc.
Added docstrings

More tidying up and correcting docstrings

More, corrected docs

sunxspex/sunxspex_fitting/likelihoods.py
@@ -1,31 +1,57 @@
+"""
+The following code contains the default log-likelihoods/fit statistics used for model fitting.
+"""
+
 import numpy as np
 from scipy.special import factorial
 
 __all__ = ["LogLikelihoods"]
 
 class LogLikelihoods:
+    """
+    This class's job is to hold all the log-likelihoods/fit statistics in the one place.
+    
+    All are to be maximised to find the best fit.
+
+    Methods
+    -------
+    remove_non_numbers : _lhoods
+            Takes an array and removes Nans, infs, -infs.
+
+    gaussian_loglikelihood : model_counts, observed_counts, observed_count_errors
+            Gaussian log-likelihood function. Access via `log_likelihoods` attribute.
+
+    chi2_loglikelihood : model_counts, observed_counts, observed_count_errors
+            Chi-squared fit statistic function. Access via `log_likelihoods` attribute.
+
+    poisson_loglikelihood : model_counts, observed_counts, observed_count_errors
+            Poissonian log-likelihood function. Access via `log_likelihoods` attribute.
+
+    cash_loglikelihood : model_counts, observed_counts, observed_count_errors
+            Cash log-likelihood function. Access via `log_likelihoods` attribute.
+
+    cstat_loglikelihood : model_counts, observed_counts, observed_count_errors
+            C-stat log-likelihood function. Access via `log_likelihoods` attribute. 
+
+    Attributes
+    ----------
+    log_likelihoods : dict
+            Name of each log-likelihoods/fit statistic (dict. key) with the method object (dict. value).
+            
+
+    _construction_string : string
+            String to be returned from __repr__() dunder method.
+
+    Examples
+    --------
+    # calculate how well a model fits data with the c-stat statistic
+    ll = LogLikelihoods()
+    model_counts, observed_counts, observed_count_errors = modelled_counts, counts_data, count_error
+    log_likelihoods_or_fit_statistic = ll.loglikelihoods["cstat"](model_counts, observed_counts, observed_count_errors)
+    """
     def __init__(self):
-        """
-        This class's job is to hold all the log-likelihoods/fit statistics in the one place.
-        All are to be maximised to find the best fit.
-
-        Parameters
-        ----------
-
-        Attributes
-        ----------
-        log_likelihoods : dict
-                Name of each log-likelihoods/fit statistic (dict. key) with the method object (dict. value).
-        _construction_string : string
-                String to be returned from __repr__() dunder method.
-
-        Examples
-        --------
-        # calculate how well a model fits data with the c-stat statistic
-        ll = LogLikelihoods()
-        model_counts, observed_counts, observed_count_errors = modelled_counts, counts_data, count_error
-        log_likelihoods_or_fit_statistic = ll.loglikelihoods["cstat"](model_counts, observed_counts, observed_count_errors)
-        """
+        """Construct a string to show how the class was constructed (`_construction_string`) and set the `log_likelihoods` dictionary attribute."""
+        
         self._construction_string = "LogLikelihoods()"
         # dictionary of all likelihood functions? E.g., 
         self.log_likelihoods = {"gaussian":self.gaussian_loglikelihood, 
@@ -34,8 +60,8 @@ class LogLikelihoods:
                                 "cash":self.cash_loglikelihood, 
                                 "cstat":self.cstat_loglikelihood}
         
-    def remove_nans(self, _lhoods):
-        ''' Removes Nans in the output array from any /0 data entries.
+    def _remove_nans(self, _lhoods):
+        """ Removes Nans in the output array from any /0 data entries.
 
         Parameters
         ----------
@@ -45,11 +71,11 @@ class LogLikelihoods:
         Returns
         -------
         Input array but with Nans removed.
-        '''
+        """
         return _lhoods[~np.isnan(_lhoods)]
     
     def remove_non_numbers(self, _lhoods):
-        ''' Removes Nans, infs, -infs in the output array from any /0 data entries.
+        """ Removes Nans, infs, -infs in the output array from any /0 data entries.
 
         Parameters
         ----------
@@ -59,13 +85,14 @@ class LogLikelihoods:
         Returns
         -------
         Input array but with Nans, infs, -infs removed.
-        '''
-        _lhoods = self.remove_nans(_lhoods)
+        """
+        _lhoods = self._remove_nans(_lhoods)
         return _lhoods[np.isfinite(_lhoods)]
     
-    def check_numbers_left(self, remaining):
-        ''' Check if there are any numbers left after all Nans, infs, and -infs have been remove 
-        and return the sum, if no numbers left then return -np.inf since it it a rubbish fit.
+    def _check_numbers_left(self, remaining):
+        """ Check if there are any numbers left after all Nans, infs, and -infs have been remove and return the sum.
+        
+        If no numbers left then return -np.inf since it it a rubbish fit.
 
         Parameters
         ----------
@@ -76,14 +103,14 @@ class LogLikelihoods:
         Returns
         -------
         Sum of the bin fit statistics, or -np.inf if nothing to sum.
-        '''
+        """
         if len(remaining)==0:
             return -np.inf
         else:
             return np.sum(remaining)
         
     def gaussian_loglikelihood(self, model_counts, observed_counts, observed_count_errors):
-        ''' Gaussian log-likelihood.
+        """ Gaussian log-likelihood.
 
         .. math::
          ln(L_{Gauss}) = -\frac{N}{2} ln(2\pi D^{2}) + \frac{1}{2}\Chi^{2}
@@ -99,15 +126,15 @@ class LogLikelihoods:
         Returns
         -------
         A float, the gaussian log-likelihood (to be maximised).
-        '''
+        """
         
         likelihoods = -(len(observed_counts)/2) * np.log(2*np.pi*np.array(observed_count_errors)**2) + (1/2)* self.chi2_loglikelihood(model_counts, observed_counts, observed_count_errors)
 
         # best value is first whole term, if the chi squared section has any value then it is always subtracted
-        return self.check_numbers_left(self.remove_non_numbers(likelihoods)) # =ln(L)
+        return self._check_numbers_left(self.remove_non_numbers(likelihoods)) # =ln(L)
     
     def chi2_loglikelihood(self, model_counts, observed_counts, observed_count_errors):
-        ''' Chi-squared fit statistic.
+        """ Chi-squared fit statistic.
 
         .. math::
          \Chi^{2} = - (\frac{(D - \mu)^{2}}{\sigma})^{2}
@@ -122,15 +149,15 @@ class LogLikelihoods:
         Returns
         -------
         A float, the chi-squared fit statistic (to be maximised).
-        '''
+        """
         
         likelihoods = -(np.array(observed_counts)-np.array(model_counts))**2 / np.array(observed_count_errors)**2
 
         # best value is 0, every other value is negative
-        return self.check_numbers_left(self.remove_non_numbers(likelihoods)) # =ln(L)
+        return self._check_numbers_left(self.remove_non_numbers(likelihoods)) # =ln(L)
     
     def poisson_loglikelihood(self, model_counts, observed_counts, observed_count_errors):
-        ''' Poissonian log-likelihood.
+        """ Poissonian log-likelihood.
 
         .. math::
          ln(L_{Poisson}) = D ln(\mu) - \mu - ln(D!)
@@ -145,18 +172,19 @@ class LogLikelihoods:
         Returns
         -------
         A float, the poissonian log-likelihood (to be maximised).
-        '''
+        """
         
         # proper Poisson log-likelihood, factorial and all
         likelihoods = np.array(observed_counts) * np.log(np.array(model_counts)) - np.array(model_counts) - np.log(factorial(observed_counts))
         
-        return self.check_numbers_left(self.remove_non_numbers(likelihoods))
+        return self._check_numbers_left(self.remove_non_numbers(likelihoods))
     
     def cash_loglikelihood(self, model_counts, observed_counts, observed_count_errors):
-        ''' Cash log-likelihood. A simplification of the poissonian log-likelihood where the 
-        independent data term is neglected. Since the data term is neglected the absolute number 
-        does not say much about the absolute goodness of fit as more observations means a higher 
-        natural number.
+        """ Cash log-likelihood. 
+        
+        A simplification of the poissonian log-likelihood where the independent data term is 
+        neglected. Since the data term is neglected the absolute number does not say much 
+        about the absolute goodness of fit as more observations means a higher natural number.
 
         .. math::
          ln(L_{Poisson}) = D ln(\mu) - \mu 
@@ -173,19 +201,21 @@ class LogLikelihoods:
         Returns
         -------
         A float, the cash log-likelihood (to be maximised).
-        '''
+        """
         
         # needs to be based on counts so multiply out the livetime and keV^-1
         # Cash - more bins=higher natural value, i.e., the number doesn't say much about the absolute goodness of fit 
         likelihoods = np.array(observed_counts) * np.log(np.array(model_counts)) - np.array(model_counts)
         
-        return self.check_numbers_left(self.remove_non_numbers(likelihoods))
+        return self._check_numbers_left(self.remove_non_numbers(likelihoods))
     
     def cstat_loglikelihood(self, model_counts, observed_counts, observed_count_errors):
-        ''' C-stat log-likelihood. A simplification of the poissonian log-likelihood where the 
-        independent data term is replaced using sterling's approximation. Used in the XSPEC fitting 
-        software. Unlike the cash statistic, the resulting value retains some meaning of the 
-        absolute goodness of fit.
+        """ C-stat log-likelihood. 
+        
+        A simplification of the poissonian log-likelihood where the independent data term 
+        is replaced using sterling's approximation. Used in the XSPEC fitting software. 
+        Unlike the cash statistic, the resulting value retains some meaning of the absolute 
+        goodness of fit.
 
         .. math::
          ln(L_{Poisson}) = D (ln(\frac{\mu}{D}) + 1) - \mu 
@@ -202,19 +232,19 @@ class LogLikelihoods:
         Returns
         -------
         A float, the c-stat log-likelihood (to be maximised).
-        '''
+        """
         
         # C-stat (XSPEC) - has data term in it so the lower the -2*ln(L) number the better the fit, this is the ln(L) though
         # Best value is 0, if obs_counts>mod_counts or obs_counts<mod_counts then likelihoods<0
         # Obvious since e^0=1 
         likelihoods = np.array(observed_counts) * ( np.log(np.array(model_counts)/np.array(observed_counts)) + 1 ) - np.array(model_counts)
 
-        return self.check_numbers_left(self.remove_non_numbers(likelihoods)) 
+        return self._check_numbers_left(self.remove_non_numbers(likelihoods)) 
 
     def __repr__(self):
-        '''Provide a representation to construct the class from scratch.'''
+        """Provide a representation to construct the class from scratch."""
         return self._construction_string
     
     def __str__(self):
-        '''Provide a printable, user friendly representation of what the class contains.'''
+        """Provide a printable, user friendly representation of what the class contains."""
         return f"Different log-likelihoods/fit statistics available are: {self.log_likelihoods}"

sunxspex/sunxspex_fitting/photon_models_for_fitting.py
@@ -1,7 +1,10 @@
+"""
+The following code contains the default functions commonly used for solar X-ray spectral fitting in the for f(*args,energies=None).
+"""
+
 import numpy as np
 from astropy import units as u
-from sys import path as sys_path
-sys_path.append('../')
+
 from sunxspex.thermal import thermal_emission
 from sunxspex.emission import bremsstrahlung_thin_target, bremsstrahlung_thick_target
 
@@ -15,8 +18,7 @@ defined_photon_models = {"f_vth":["T", "EM"],
                          "thick_warm":["tot_eflux", "indx", "ec", "plasmaD", "loopT", "length"]}
 
 def f_vth(temperature, emission_measure46, energies=None):
-    ''' Calculates optically thin thermal bremsstrahlung radiation as seen 
-    from Earth. 
+    """ Calculates optically thin thermal bremsstrahlung radiation as seen from Earth. 
 
     [1] https://hesperia.gsfc.nasa.gov/ssw/packages/xray/idl/f_vth.pro
 
@@ -25,8 +27,10 @@ def f_vth(temperature, emission_measure46, energies=None):
     energies : 2d array
             Array of energy bins for the model to be calculated over. 
             E.g., [[1,1.5],[1.5,2],[2,2.5],...].
+
     temperature : int or float
             Plasma temperature in megakelvin.
+
     emission_measure46 : int or float
             Emission measure in units of 1e46 cm^-3.
 
@@ -34,15 +38,15 @@ def f_vth(temperature, emission_measure46, energies=None):
     -------
     A 1d array of optically thin thermal bremsstrahlung radiation in units 
     of ph s^-1 keV^-1.
-    '''
+    """
+            
     energies = np.unique(np.array(energies).flatten()) << u.keV # turn [[1,2],[2,3],[3,4]] into [1,2,3,4]
     temperature = temperature*1e6 << u.K
     emission_measure = emission_measure46*1e46 << u.cm**(-3)
     return thermal_emission(energies,temperature,emission_measure).value
 
 def thick_fn(total_eflux, index, e_c, energies=None):
-    ''' Calculates the thick-target bremsstrahlung radiation of a 
-    single power-law electron distribution.
+    """ Calculates the thick-target bremsstrahlung radiation of a single power-law electron distribution.
 
     [1] Brown, Solar Physics 18, 489 (1971) (https://link.springer.com/article/10.1007/BF00149070)
     [2] https://hesperia.gsfc.nasa.gov/ssw/packages/xray/doc/brm_thick_doc.pdf
@@ -53,10 +57,13 @@ def thick_fn(total_eflux, index, e_c, energies=None):
     energies : 2d array
             Array of energy bins for the model to be calculated over. 
             E.g., [[1,1.5],[1.5,2],[2,2.5],...].
+
     total_eflux : int or float
             Total integrated electron flux, in units of 10^35 e^- s^-1.
+
     index : int or float
             Power-law index of the electron distribution.
+
     e_c : int or float
             Low-energy cut-off of the electron distribution in units of keV.
 
@@ -64,7 +71,7 @@ def thick_fn(total_eflux, index, e_c, energies=None):
     -------
     A 1d array of thick-target bremsstrahlung radiation in units 
     of ph s^-1 keV^-1.
-    '''
+    """
     
     hack = np.round([total_eflux, index, e_c], 15)
     total_eflux, index, e_c = hack[0],  hack[1],  hack[2]
@@ -86,8 +93,7 @@ def thick_fn(total_eflux, index, e_c, energies=None):
 
 
 def thick_warm(total_eflux, index, e_c, plasmaD, T, length, energies=None):
-    ''' Calculates the warm thick-target bremsstrahlung radiation as seen 
-    from Earth.
+    """ Calculates the warm thick-target bremsstrahlung radiation as seen from Earth.
 
     [1] Kontar et al, ApJ 2015 (http://adsabs.harvard.edu/abs/2015arXiv150503733K)
     [2] https://hesperia.gsfc.nasa.gov/ssw/packages/xray/idl/f_thick_warm.pro
@@ -97,16 +103,22 @@ def thick_warm(total_eflux, index, e_c, plasmaD, T, length, energies=None):
     energies : 2d array
             Array of energy bins for the model to be calculated over. 
             E.g., [[1,1.5],[1.5,2],[2,2.5],...].
+
     total_eflux : int or float
             Total integrated electron flux in units of 10^35 e^- s^-1.
+
     index : int or float
             Power-law index of the electron distribution.
+
     e_c : int or float
             Low-energy cut-off of the electron distribution in units of keV.
+
     plasmaD : int or float
             Plasma number density in units of 1e10 cm^-3.
+
     T : int or float
             Plasma temperature in megakelvin.
+
     length : int or float
             Plasma column length (usually half the full loop length?) in Mm.
 
@@ -114,7 +126,7 @@ def thick_warm(total_eflux, index, e_c, plasmaD, T, length, energies=None):
     -------
     A 1d array of the warm thick-target bremsstrahlung radiation in units 
     of ph s^-1 keV^-1.
-    '''
+    """
 
     me_keV = 511 #[keV]
     cc = 2.99e10  # speed of light [cm/s]