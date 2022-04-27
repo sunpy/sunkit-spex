703871ddf07a74c7c6423e5f0195d344adf33884
Shane Maloney <shane.maloney@tcd.ie>
2022-04-27 12:24:08 +0100

    Add required imports back (removed by isort).

--- sunxspex/__init__.py
+++ sunxspex/__init__.py
@@ -1,12 +1,7 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 
-<<<<<<< HEAD
-from .version import __version__
-
-=======
 
 from .version import __version__
->>>>>>> 1664cc0 (Apply suggestions from code review)
 
 __all__ = []
 from . import io, sunxspex_fitting, thermal
