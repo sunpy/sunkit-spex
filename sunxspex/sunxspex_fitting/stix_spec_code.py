1ea95e97194b74d19bfeb2ac61e1748dd5221e99
natsat0919 <natalia.bajnok@gmail.com>
2023-06-02 16:46:13 +0100

    STIX specs_times refactoring

--- sunxspex/sunxspex_fitting/stix_spec_code.py
+++ sunxspex/sunxspex_fitting/stix_spec_code.py
@@ -28,8 +28,8 @@ def _get_spec_file_info(spec_file):
     """
     sdict = io._read_stix_spec_file(spec_file)
 
-    times_mids = _ds_times2s_times(sdict["2"][1]["time"])   # mid-times of spectra, entries -> times. Mid-times from start of observation
-    time_deltas = _ds_times2s_times(sdict["2"][1]["timedel"])  # times deltas of spectra, entries -> times
+    times_mids = sdict["2"][1]["time"]  # mid-times of spectra, entries -> times. Mid-times from start of observation
+    time_deltas = sdict["2"][1]["timedel"]  # times deltas of spectra, entries -> times
     time_diff_so2e = sdict["0"][0]["EAR_TDEL"]  # time difference between Sun2Earth and Sun2SO, so time at earth for measurement is this difference added on to the actual detection time
     # spectrum number in the file, entries -> times # spec_num = sdict["1"][1]["SPEC_NUM"]
 
@@ -40,8 +40,8 @@ def _get_spec_file_info(spec_file):
     _plus_half_bin_width = np.ceil(time_deltas/2)
     t_hi = times_mids + _plus_half_bin_width
 
-    spec_stimes = [Time(sdict["0"][0]["DATE-BEG"], format='isot', scale='utc')+TimeDelta(time_diff_so2e * u.s)+TimeDelta(dt * u.s) for dt in t_lo]
-    spec_etimes = [Time(sdict["0"][0]["DATE-BEG"], format='isot', scale='utc')+TimeDelta(time_diff_so2e * u.s)+TimeDelta(dt * u.s) for dt in t_hi]
+    spec_stimes = [Time(sdict["0"][0]["DATE-BEG"], format='isot', scale='utc')+TimeDelta(time_diff_so2e * u.s)+TimeDelta(dt * u.cs) for dt in t_lo]
+    spec_etimes = [Time(sdict["0"][0]["DATE-BEG"], format='isot', scale='utc')+TimeDelta(time_diff_so2e * u.s)+TimeDelta(dt * u.cs) for dt in t_hi]
     time_bins = np.concatenate((np.array(spec_stimes)[:, None], np.array(spec_etimes)[:, None]), axis=1)
 
     channel_bins_inds, channel_bins = _return_masked_bins(sdict)
@@ -52,21 +52,6 @@ def _get_spec_file_info(spec_file):
     counts, counts_err, cts_rates, cts_rate_err = _spec_file_units_check(stix_dict=sdict, time_dels=time_deltas)
 
     return channel_bins, channel_bins_inds, time_bins, lvt, counts, counts_err, cts_rates, cts_rate_err
-
-
-def _ds_times2s_times(ds_times):
-    """ STIX time might be in ds (deci-seconds). Convert to seconds.
-
-    Parameters
-    ----------
-    ds_times : time bins
-            A 2D array of time bins.
-
-    Returns
-    -------
-    A 2d array of the time bin edges.
-    """
-    return ds_times * (10**(-2))
 
 
 def _return_masked_bins(sdict):
