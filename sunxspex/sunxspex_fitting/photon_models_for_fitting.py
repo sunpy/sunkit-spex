c71a61ad5edc7823cfca45b12c90ade0a83ed2d3
William Setterberg <william.s.setterberg@gmail.com>
2023-10-18 17:08:51 -0500

    fix thick_fn upper bounds (#115)
    * Upper bound of thick target emission function is now calculated from highest energy input.

--- sunxspex/sunxspex_fitting/photon_models_for_fitting.py
+++ sunxspex/sunxspex_fitting/photon_models_for_fitting.py
@@ -14,8 +14,6 @@ from ..thermal import thermal_emission
 
 __all__ = ["defined_photon_models", "f_vth", "thick_fn", "thick_warm"]
 
-# Issue when using np.float64 numbers for the parameters as it ends up returning all nans and infs but rounding to 15 decimal places fixes this??????
-
 # The defined models shouldn't have duplicate parameter input names
 defined_photon_models = {"f_vth": ["T", "EM"],
                          "thick_fn": ["total_eflux", "index", "e_c"],
@@ -45,9 +43,10 @@ def f_vth(temperature, emission_measure46, energies=None):
     of ph s^-1 cm^-2 keV^-1.
     """
 
-    energies = np.unique(np.array(energies).flatten()) << u.keV  # turn [[1,2],[2,3],[3,4]] into [1,2,3,4]
-    temperature = temperature*1e6 << u.K
-    emission_measure = emission_measure46*1e46 << u.cm**(-3)
+    # turn [[1,2],[2,3],[3,4]] into [1,2,3,4]
+    energies = np.unique(np.array(energies).flatten()) << u.keV
+    temperature = temperature * 1e6 << u.K
+    emission_measure = emission_measure46 * 1e46 << u.cm**(-3)
     return thermal_emission(energies, temperature, emission_measure).value
 
 
@@ -80,22 +79,28 @@ def thick_fn(total_eflux, index, e_c, energies=None):
     """
 
     hack = np.round([total_eflux, index, e_c], 15)
-    total_eflux, index, e_c = hack[0],  hack[1],  hack[2]
+    total_eflux, index, e_c = hack[0], hack[1], hack[2]
 
     energies = np.mean(energies, axis=1)  # since energy bins are given, use midpoints though
 
-    # total_eflux in units of 1e35 e/s
-    # single power law so set eebrk==eehigh at a high value, high q also
-    output = bremsstrahlung_thick_target(photon_energies=energies,
-                                         p=index,
-                                         eebrk=150,
-                                         q=20,
-                                         eelow=e_c,
-                                         eehigh=150)*total_eflux*1e35
+    # we want a single power law electron distribution,
+    # so set eebrk == eehigh at a high value.
+    # we don't care about q at E > eebrk.
+    high_break = energies.max() * 10
+    output = bremsstrahlung_thick_target(
+        photon_energies=energies,
+        p=index,
+        eebrk=high_break,
+        q=20,
+        eelow=e_c,
+        eehigh=high_break
+    )
 
     output[np.isnan(output)] = 0
     output[~np.isfinite(output)] = 0
-    return output
+
+    # convert to 1e35 e-/s
+    return output * total_eflux * 1e35
 
 
 def thick_warm(total_eflux, index, e_c, plasma_d, loop_temp, length, energies=None):
@@ -137,24 +142,27 @@ def thick_warm(total_eflux, index, e_c, plasma_d, loop_temp, length, energies=No
     ME_KEV = 511  # [keV]
     CC = 2.99e10  # speed of light [cm/s]
     KK = 2.6e-18
-    n_p = plasma_d*1e10  # was in 1e10 cm^-3, now in cm^-3
-    tloop = loop_temp*8.6173e-2  # was in MK, now in keV
-    l = length*1e8  # was in Mm, now in cm
+    n_p = plasma_d * 1e10  # was in 1e10 cm^-3, now in cm^-3
+    tloop = loop_temp * 8.6173e-2  # was in MK, now in keV
+    l = length * 1e8  # was in Mm, now in cm
 
-    ll = tloop**2/(2*KK*n_p)  # collisional stopping distance for electrons of Tloop energy
+    ll = tloop**2 / (2 * KK * n_p)  # collisional stopping distance for electrons of Tloop energy
 
-    emin = tloop*3*(5*ll/l)**4
+    emin = tloop * 3 * (5 * ll / l)**4
 
     if emin > 0.1:
-        print(f"The loop_temp ({loop_temp}), plasma density ({plasma_d}), and loop length ({length}) make emin ({emin}) >0.1. Fixing emin to 0.1.")
+        print(
+            f"The loop_temp ({loop_temp}), plasma density ({plasma_d}), and loop length ({length}) make emin ({emin}) >0.1. Fixing emin to 0.1.")
         emin = 0.1
 
-    lmin = e_c**2 / (2*KK*n_p) / 3
+    lmin = e_c**2 / (2 * KK * n_p) / 3
     if lmin > l:
         print("Minimum length>length")
 
-    em_add = 3*np.pi/2/KK/CC*np.sqrt(ME_KEV/8.)*tloop**2/np.sqrt(emin)*total_eflux*1e35
+    em_add = 3 * np.pi / 2 / KK / CC * \
+        np.sqrt(ME_KEV / 8.) * tloop**2 / np.sqrt(emin) * total_eflux * 1e35
 
-    em46 = em_add*1e-46  # get EM in units of 10^46 cm^(-3)
+    em46 = em_add * 1e-46  # get EM in units of 10^46 cm^(-3)
 
-    return thick_fn(total_eflux, index, e_c, energies=energies) + f_vth(loop_temp, em46, energies=energies)
+    return thick_fn(total_eflux, index, e_c, energies=energies) + \
+        f_vth(loop_temp, em46, energies=energies)
