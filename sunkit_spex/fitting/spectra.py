f58f4fdf06420de82c6f3ad8516c3eb39ac83a2e
William Setterberg <sette095@umn.edu>
2024-03-02 08:39:59 -0600

    spectra: remove unused class and add properties

--- sunkit_spex/fitting/spectra.py
+++ sunkit_spex/fitting/spectra.py
@@ -1,4 +1,5 @@
 import astropy.units as u
+from astropy.utils import lazyproperty
 from dataclasses import dataclass
 import numpy as np
 
@@ -15,24 +16,10 @@ class XraySpectrum:
     response_matrix: (u.cm**2 * u.ct / u.ph) # type: ignore
     effective_exposure: u.s
 
+    @lazyproperty
+    def counts_de(self) -> u.keV:
+        return np.diff(self.count_energy_edges)
 
-@dataclass
-class StrippedSpectrum:
-    ''' Take units off of a counts spectrum '''
-    counts: np.ndarray
-    counts_error: np.ndarray
-    count_energy_edges: np.ndarray
-    photon_energy_edges: np.ndarray
-    response_matrix: np.ndarray
-    effective_exposure: np.ndarray
-
-    @classmethod
-    def from_xray_spectrum(cls, with_units: XraySpectrum):
-        return cls(
-            with_units.counts.to_value(u.ct),
-            with_units.counts_error.to_value(u.ct),
-            with_units.count_energy_edges.to_value(u.keV),
-            with_units.photon_energy_edges.to_value(u.keV),
-            with_units.response_matrix.to_value(u.cm**2 * u.ct / u.ph),
-            with_units.effective_exposure.to_value(u.s)
-        )
+    @lazyproperty
+    def photons_de(self) -> u.keV:
+        return np.diff(self.photon_energy_edges)
