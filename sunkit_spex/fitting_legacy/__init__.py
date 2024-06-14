8021cd60cb9a291b80272228405f95b75842e333
William Setterberg <sette095@umn.edu>
2024-06-14 02:06:29 -0500

    Rhessi srm editable (post rename) (#143)
    * editable rhessi SRM (sorry big diff)
    
    * new rhessi example (delete old one)
    
    * delete unused io files
    
    * move RHESSI loader to extern
    
    * update docs index and changelog
    
    * update example to extern import
    
    * pre-commit works (fix strings)
    
    * Update sunkit_spex/fitting_legacy/instruments.py
    
    ---------
    
    Co-authored-by: DanRyanIrish <ryand5@tcd.ie>

--- sunkit_spex/fitting_legacy/__init__.py
+++ sunkit_spex/fitting_legacy/__init__.py
@@ -8,6 +8,4 @@ from . import (
     parameter_handler,
     photon_models_for_fitting,
     rainbow_text,
-    rhes_spec_code,
-    stix_spec_code,
 )
--- sunkit_spex/fitting_legacy/io.py
+++ sunkit_spex/fitting_legacy/io.py
@@ -1,12 +1,10 @@
 """
 The ``io`` module contains code to read instrument specific spectral data.
 """
-import numpy as np
 
 from astropy.io import fits
 
-__all__ = ["_read_pha", "_read_arf", "_read_rmf", "_read_rhessi_spec_file", "_read_rhessi_srm_file",
-           "_read_stix_spec_file", "_read_stix_srm_file"]
+__all__ = ["_read_pha", "_read_arf", "_read_rmf"]
 
 
 def _read_pha(file):
@@ -71,92 +69,3 @@ def _read_rmf(file):
         data = hdul[2].data
 
     return data['energ_lo'], data['energ_hi'], data['n_grp'], data['f_chan'], data['n_chan'], data['matrix']
-
-
-def _read_rhessi_spec_file(spec_file):
-    """
-    Read RHESSI spectral fits file and extract useful information from it.
-
-    Parameters
-    ----------
-    spec_file :  `str`, `file-like` or `pathlib.Path`
-        A RHESSI spectral fits file (see `~astropy.fits.io.open` for details)
-
-    Returns
-    -------
-    `dict`
-        RHESSI spectal data
-    """
-    rdict = {}
-    with fits.open(spec_file) as hdul:
-        for i in range(4):
-            rdict[str(i)] = [hdul[i].header, hdul[i].data]
-    return rdict
-
-
-def _read_rhessi_srm_file(srm_file):
-    """
-    Read RHESSI SRM fits file and extract useful information from it.
-
-    Parameters
-    ----------
-    srm_file : `str`, `file-like` or `pathlib.Path`
-        A RHESSI SRM fits file (see `~astropy.fits.io.open` for details)
-
-    Returns
-    -------
-    `dict`
-        RHESSI SRM data
-    """
-    srmrdict = {}
-    with fits.open(srm_file) as hdul:
-        for i in range(4):
-            srmrdict[str(i)] = [hdul[i].header, hdul[i].data]
-    return srmrdict
-
-
-def _read_stix_spec_file(spec_file):
-    """
-    Read STIX spectral fits file and extracts useful information from it.
-
-    Parameters
-    ----------
-    spec_file : `str`, `file-like` or `pathlib.Path`
-            STIX spectral fits file (see `~astropy.fits.io.open` for details)
-
-    Returns
-    -------
-    `dict`
-        STIX spectral data.
-    """
-    sdict = {}
-    with fits.open(spec_file) as hdul:
-        for i in range(5):
-            sdict[str(i)] = [hdul[i].header, hdul[i].data]
-    return sdict
-
-
-def _read_stix_srm_file(srm_file):
-    """
-    Read a STIX SRM spectral fits file and extract useful information from it.
-
-    Parameters
-    ----------
-    srm_file : `str` or `pathlib.Path`
-        STIX SRM fits file
-
-    Returns
-    -------
-    `dict`
-        STIX SRM data (photon bins, count bins, and SRM in units of [counts/keV/photons]).
-    """
-    with fits.open(srm_file) as hdul:
-        d0 = hdul[1].header
-        d1 = hdul[1].data
-        d3 = hdul[2].data
-
-    pcb = np.concatenate((d1['ENERG_LO'][:, None], d1['ENERG_HI'][:, None]), axis=1)
-
-    return {"photon_energy_bin_edges": pcb,
-            "count_energy_bin_edges": np.concatenate((d3['E_MIN'][:, None], d3['E_MAX'][:, None]), axis=1),
-            "drm": d1['MATRIX']*d0["GEOAREA"]}
