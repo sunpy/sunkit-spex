42c2e3070d89d058b0c2031ef32faa00651be8f3
DanRyanIrish <ryand5@tcd.ie>
2024-06-14 08:55:34 +0200

    Update sunkit_spex/fitting_legacy/instruments.py

--- sunkit_spex/fitting_legacy/instruments.py
+++ sunkit_spex/fitting_legacy/instruments.py
@@ -49,7 +49,7 @@ class InstrumentBlueprint:
     as **kwargs.
 
     The `DataLoader` class in data_loader.py then creates a dictionary attrbute called `loaded_spec_data`
-    (note no underscore) that is then getable by the user when spectral fitting with the `SunXspex` class
+    (note no underscore) that is then getable by the user when spectral fitting with the `Fitter` class
     in fitter.py where the keys are each spectum's ID (e.g, spectrum1, spectrum2, etc.).
     """
 
