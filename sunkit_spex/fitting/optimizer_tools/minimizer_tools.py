a26ba9e2ed1eaa3feaab229844ccab731b068308
Kristopher Cooper <kris@infra04-wg012.eduroam.wireless.umn.edu>
2024-06-17 13:05:00 -0500

    Added docstrings.

--- sunkit_spex/fitting/optimizer_tools/minimizer_tools.py
+++ sunkit_spex/fitting/optimizer_tools/minimizer_tools.py
@@ -1,10 +1,35 @@
+from scipy.optimize import minimize
 
+def scipy_minimize(objective_func, param_guesses, args, **kwargs):
+    """ A function to optimize fitted parameters to data.
+    
+    Parameters
+    ----------
+    objective_func : `function`
+        The function to be optimized.
 
-def scipy_minimize(objective_func, param_guesses, *args, **kwargs):
+    param_guesses : `ndarray`
+        Initial guesses of the independent variables.
 
-    method = kwargs.get("method", "Nelder-Mead")
+    args : 
+        Any arguments required to be passed to the objective function
+        after the param_guesses. 
+        E.g., `objective_func(param_guesses, *args)`.
+
+    kwargs : 
+        Passed to `scipy.optimize.minimize`.
+        A default value for the method is chosen to be "Nelder-Mead".
+
+    Returns
+    -------
+    `scipy.optimize.OptimizeResult`
+        The optimized result after comparing the model to the data.
+    """
+
+    method = kwargs.pop("method", "Nelder-Mead")
 
     return minimize(objective_func, 
-               param_guesses, 
-               args=args,
-               method=method)
+                    param_guesses, 
+                    args=args,
+                    method=method, 
+                    **kwargs)
--- sunkit_spex/fitting/statistics/gaussian.py
+++ sunkit_spex/fitting/statistics/gaussian.py
@@ -5,5 +5,18 @@ def chi_squared(data_y, model_y):
     The form to optimise while fitting. 
     
     * No error included here. *
+
+    Parameters
+    ----------
+    data_y : `ndarray`
+        The data to be fitted.
+    
+    model_y : `ndarray`
+        The model values being fitted.
+
+    Returns
+    -------
+    `float`
+        The value to be optimized that compares the model to the data.
     """
     return np.sum((data_y - model_y)**2)
