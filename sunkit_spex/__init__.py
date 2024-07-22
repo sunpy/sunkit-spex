4f952995bf93d2f9a0903401693f7abb6b15c222
CyclingNinja <bennett.sm89@gmail.com>
2024-07-22 14:28:06 +0200

    Adding spex custom settings

--- sunkit_spex/__init__.py
+++ sunkit_spex/__init__.py
@@ -1,4 +1,7 @@
 
-from .version import version as __version__
+from .version import __version__
 
 __all__ = []
+from . import extern
+from .legacy import fitting_legacy
+from .models.physical import io, thermal
