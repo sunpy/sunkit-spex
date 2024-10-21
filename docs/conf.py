d4b39b83e6b3eaec9638ec5ef70760415dfc43d1
Stuart Mumford <stuart@cadair.com>
2024-10-21 08:28:11 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,6 @@ intersphinx_mapping = {
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-
 html_theme = "sunpy"
 
 # Render inheritance diagrams in SVG
@@ -104,7 +103,17 @@ graphviz_dot_args = [
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
 
+# By default, when rendering docstrings for classes, sphinx.ext.autodoc will
+# make docs with the class-level docstring and the class-method docstrings,
+# but not the __init__ docstring, which often contains the parameters to
+# class constructors across the scientific Python ecosystem. The option below
+# will append the __init__ docstring to the class-level docstring when rendering
+# the docs. For more options, see:
+# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autoclass_content
+autoclass_content = "both"
+
 # -- Options for the Sphinx gallery -------------------------------------------
+
 path = pathlib.Path.cwd()
 example_dir = path.parent.joinpath("examples")
 sphinx_gallery_conf = {
@@ -117,14 +126,5 @@ sphinx_gallery_conf = {
     "remove_config_comments": True,
     "only_warn_on_example_error": True,
 }
-
-# By default, when rendering docstrings for classes, sphinx.ext.autodoc will
-# make docs with the class-level docstring and the class-method docstrings,
-# but not the __init__ docstring, which often contains the parameters to
-# class constructors across the scientific Python ecosystem. The option below
-# will append the __init__ docstring to the class-level docstring when rendering
-# the docs. For more options, see:
-# https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autoclass_content
-autoclass_content = "both"
 
 # -- Other options ----------------------------------------------------------
