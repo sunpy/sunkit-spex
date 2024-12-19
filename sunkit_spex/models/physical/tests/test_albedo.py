f7fc55d37b26580af7e0aaf24adf183e22b82d80
natsat0919 <natalia.bajnok@gmail.com>
2024-12-19 19:38:38 +0000

    Speed up albedo correction, add AlbedoModel

--- sunkit_spex/models/physical/tests/test_albedo.py
+++ sunkit_spex/models/physical/tests/test_albedo.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-import astropy.units as u
-
 from sunkit_spex.models.physical.albedo import albedo
 
 
@@ -9,5 +7,8 @@ def test_albedo():
     e = np.linspace(4, 600, 597)
     e_c = e[:-1] + np.diff(e)
     s = 125 * e_c**-3
-    out = albedo(s, e * u.keV, 45 * u.deg)
+    # e = e *u.keV
+    theta = 0.2  # u.rad
+    albedo_matrix = albedo(e, theta=theta)
+    out = s + s @ albedo_matrix
     print(out)
