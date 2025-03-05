5f9d3b7dfd8921bf31bef842bed2b7ba13a04c87
jajmitchell <jmitchell@aip.de>
2025-03-05 14:58:28 +0100

    Pre-commit
    Have run pre-commit

--- sunkit_spex/models/scaling.py
+++ sunkit_spex/models/scaling.py
@@ -1,24 +1,14 @@
-from functools import lru_cache
-
-import numpy as np
-from numpy.typing import NDArray
-from scipy.interpolate import RegularGridInterpolator
-from scipy.io import readsav
-
 import astropy.units as u
 from astropy.modeling import FittableModel, Parameter
 from astropy.units import Quantity
 
-from sunpy.data import cache
-
-__all__ = ["DistanceScale", "Constant"]
+__all__ = ["Constant", "DistanceScale"]
 
 
 class DistanceScale(FittableModel):
-
     n_inputs = 1
     n_outputs = 1
-    
+
     observer_distance = Parameter(
         name="observer_distance",
         default=1,
@@ -30,19 +20,18 @@ class DistanceScale(FittableModel):
     _input_units_allow_dimensionless = True
 
     def evaluate(self, spectrum, observer_distance):
-
-        spectrum_distance_corrected = distance_correction(spectrum, observer_distance) 
+        spectrum_distance_corrected = distance_correction(spectrum, observer_distance)
 
         return spectrum_distance_corrected
 
     def _parameter_units_for_data_units(self, inputs_unit, outputs_unit):
         return {"observer_distance": u.AU}
 
-class Constant(FittableModel):
 
+class Constant(FittableModel):
     n_inputs = 1
     n_outputs = 1
-    
+
     constant = Parameter(
         name="constant",
         default=1,
@@ -53,17 +42,15 @@ class Constant(FittableModel):
     _input_units_allow_dimensionless = True
 
     def evaluate(self, spectrum, constant):
-
         return spectrum * constant
 
 
 def distance_correction(spectrum, observer_distance):
-
     if isinstance(observer_distance, Quantity):
-        AU_distance_cm = 1*u.AU.to(u.cm)
+        AU_distance_cm = 1 * u.AU.to(u.cm)
         observer_distance_cm = observer_distance.to(u.cm)
     else:
-        AU_distance_cm = 1.496e+13
+        AU_distance_cm = 1.496e13
         observer_distance_cm = observer_distance * AU_distance_cm
 
     scale = AU_distance_cm**2 / observer_distance_cm**2
@@ -71,4 +58,3 @@ def distance_correction(spectrum, observer_distance):
     flux = spectrum * scale
 
     return flux
-
