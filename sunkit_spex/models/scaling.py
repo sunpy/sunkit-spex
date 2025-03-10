2f8c043516283f82f8189437df00906256e4eede
jajmitchell <jmitchell@aip.de>
2025-03-10 11:58:09 +0100

    Allow any length input then convert to AU
    Add an ´init´ function that will check that obvserver_distance is an Astropy quantity compatible with AU

--- sunkit_spex/models/scaling.py
+++ sunkit_spex/models/scaling.py
@@ -11,13 +11,22 @@ class DistanceScale(FittableModel):
 
     observer_distance = Parameter(
         name="observer_distance",
-        default=1,
-        unit=u.AU,
+        default=1*u.AU,
         description="Distance to the observer in AU",
         fixed=True,
     )
 
     _input_units_allow_dimensionless = True
+
+    def __init__(self,observer_distance, *args, **kwargs):
+        
+        if observer_distance.unit.is_equivalent(u.AU):
+            observer_distance = observer_distance.to(u.AU)
+        else:
+            raise ValueError('Observer distance input must be an Astropy length convertible to AU.')
+            
+        super().__init__(observer_distance, *args, **kwargs)
+
 
     def evaluate(self, spectrum, observer_distance):
         return distance_correction(spectrum, observer_distance)
@@ -48,7 +57,7 @@ def distance_correction(spectrum, observer_distance):
         AU_distance_cm = 1 * u.AU.to(u.cm)
         observer_distance_cm = observer_distance.to(u.cm)
     else:
-        AU_distance_cm = 1.496e13
+        AU_distance_cm = 1 * u.AU.to(u.cm).value
         observer_distance_cm = observer_distance * AU_distance_cm
 
     scale = AU_distance_cm**2 / observer_distance_cm**2
