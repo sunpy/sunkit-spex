f85b5d253bead02508dbad8b97410ce65ba55bf1
jajmitchell <jmitchell@aip.de>
2025-03-24 15:58:41 +0100

    Adds test to check observer_distance units
    Adds test to check observer_distance units

---
+++ sunkit_spex/models/tests/test_scaling.py
@@ -0,0 +1,10 @@
+import pytest
+
+import astropy.units as u
+
+from sunkit_spex.models import scaling
+
+
+def test_input_units_observer_distance():
+    with pytest.raises(ValueError, match="Observer distance input must be an Astropy length convertible to AU."):
+        scaling.InverseSquareFluxScaling(observer_distance=1 * u.keV)
