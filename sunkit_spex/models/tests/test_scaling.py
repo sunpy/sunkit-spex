4cfb0487255b2d154f23f6bffab9bf86ec12f951
jajmitchell <jmitchell@aip.de>
2025-04-08 09:59:43 +0200

    Reformatted
    Reformatted

--- sunkit_spex/models/tests/test_scaling.py
+++ sunkit_spex/models/tests/test_scaling.py
@@ -1,12 +1,9 @@
-import warnings
-
 import numpy as np
 import pytest
 
 import astropy.units as u
 
 from sunkit_spex.models import scaling
-
 
 
 def inverse_square_flux_scaling():
@@ -24,8 +21,9 @@ def inverse_square_flux_scaling():
     energy_edges = np.arange(3, 28.5, 0.5) * u.keV
     observer_distance = (1 * u.AU).to(u.cm)
     inputs = energy_edges
-    expected_output = np.full(len(energy_edges)-1, 3.55581626e-28) * (1 / u.cm**2)
-    return [observer_distance,inputs], expected_output
+    expected_output = np.full(len(energy_edges) - 1, 3.55581626e-28) * (1 / u.cm**2)
+    return [observer_distance, inputs], expected_output
+
 
 def constant_scaling():
     """Define expected constant scaling values.
@@ -42,8 +40,9 @@ def constant_scaling():
     energy_edges = np.arange(3, 28.5, 0.5) * u.keV
     inputs = energy_edges
     constant_value = 0.5
-    expected_output = np.full(len(energy_edges)-1, 0.5) 
-    return  [constant_value,inputs], expected_output
+    expected_output = np.full(len(energy_edges) - 1, 0.5)
+    return [constant_value, inputs], expected_output
+
 
 @pytest.mark.parametrize("distance", [inverse_square_flux_scaling])
 def test_inverse_square_flux_scaling_class_against_expected_output(distance):
@@ -51,13 +50,14 @@ def test_inverse_square_flux_scaling_class_against_expected_output(distance):
     output = scaling.InverseSquareFluxScaling(observer_distance=[input_args[0]])(input_args[1])
     np.testing.assert_allclose(output, expected, rtol=0.03)
 
+
 @pytest.mark.parametrize("constant", [constant_scaling])
 def test_constant_scaling_class_against_expected_output(constant):
     input_args, expected = constant()
     output = scaling.Constant(constant=[input_args[0]])(input_args[1])
     np.testing.assert_allclose(output, expected, rtol=0.03)
 
+
 def test_input_units_observer_distance():
     with pytest.raises(ValueError, match="Observer distance input must be an Astropy length convertible to AU."):
         scaling.InverseSquareFluxScaling(observer_distance=1 * u.keV)(np.linspace(3, 28, 100))
-
