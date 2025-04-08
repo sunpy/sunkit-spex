94d60a0939bc25b48c23a33ba2af17798d1b634e
jajmitchell <jmitchell@aip.de>
2025-04-08 11:51:47 +0200

    Adds docstrings
    Adds docstrings

--- sunkit_spex/models/scaling.py
+++ sunkit_spex/models/scaling.py
@@ -4,10 +4,68 @@ import astropy.units as u
 from astropy.modeling import FittableModel, Parameter
 from astropy.units import Quantity
 
-__all__ = ["Constant", "InverseSquareFluxScaling"]
+__all__ = ["ScalarConstant", "InverseSquareFluxScaling"]
 
 
 class InverseSquareFluxScaling(FittableModel):
+    """
+    InverseSqaureFluxScaling model converts luminosity output of physical models to a distance scaled flux.
+
+    Parameters
+    ==========
+    energy_edges :
+        Energy edges associated with input spectrum
+    observer_distance:
+        Distance of the observer from the source. 
+
+    Examples
+    ========
+    .. plot::
+        :include-source:
+
+        import astropy.units as u
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        from astropy.modeling.powerlaws import PowerLaw1D
+        from astropy.visualization import quantity_support
+
+
+        from sunkit_spex.models.scaling import InverseSquareFluxScaling
+        from astropy.modeling import Parameter, FittableModel
+
+        ph_energies = np.arange(4, 100, 0.5) 
+        ph_energies_centers = ph_energies[:-1] + 0.5*np.diff(ph_energies)
+
+        class StraightLineModel(FittableModel):
+
+            n_inputs = 1
+            n_outputs = 1
+
+            slope = Parameter(name="slope",default=1, description="Gradient of a straight line model.")
+            intercept = Parameter(name="intercept",default=0, description="Y-intercept of a straight line model.")
+
+            _input_units_allow_dimensionless = True
+
+            @staticmethod
+            def evaluate(x, slope, intercept):
+                return  intercept*x[:-1]**slope * u.ph*u.s**-1*u.keV**-1
+            
+        sim_cont = {"slope": -2, "intercept": 100}
+
+        source = StraightLineModel(**sim_cont)
+
+        with quantity_support():
+            plt.figure()
+            for i, d in enumerate([0.25,0.5,1]):
+                distance =  InverseSquareFluxScaling(observer_distance=d*u.AU)
+                observed = source * distance    
+                plt.plot(ph_energies_centers ,  observed(ph_energies), label='D = '+str(d)+' AU')
+            plt.loglog()
+            plt.legend()
+            plt.show()
+
+    """
     n_inputs = 1
     n_outputs = 1
 
@@ -33,7 +91,63 @@ class InverseSquareFluxScaling(FittableModel):
         return {"observer_distance": u.AU}
 
 
-class Constant(FittableModel):
+class ScalarConstant(FittableModel):
+    """
+    ScalarConstant 
+    model converts luminosity output of physical models to a distance scaled flux.
+
+    Parameters
+    ==========
+    energy_edges :
+        Energy edges associated with input spectrum
+
+    Examples
+    ========
+    .. plot::
+        :include-source:
+
+        import astropy.units as u
+        import numpy as np
+        import matplotlib.pyplot as plt
+
+        from astropy.modeling.powerlaws import PowerLaw1D
+        from astropy.visualization import quantity_support
+
+        from sunkit_spex.models.scaling import ScalarConstant
+        from astropy.modeling import Parameter, FittableModel
+
+        ph_energies = np.arange(4, 100, 0.5) 
+        ph_energies_centers = ph_energies[:-1] + 0.5*np.diff(ph_energies)
+
+        class StraightLineModel(FittableModel):
+
+            n_inputs = 1
+            n_outputs = 1
+
+            slope = Parameter(name="slope",default=1, description="Gradient of a straight line model.")
+            intercept = Parameter(name="intercept",default=0, description="Y-intercept of a straight line model.")
+
+            _input_units_allow_dimensionless = True
+
+            @staticmethod
+            def evaluate(x, slope, intercept):
+                return  intercept*x[:-1]**slope * u.ph*u.s**-1*u.keV**-1
+            
+        sim_cont = {"slope": -2, "intercept": 100}
+
+        source = StraightLineModel(**sim_cont)
+
+        with quantity_support():
+            plt.figure()
+            for i, c in enumerate([0.25,0.5,1,2,4]):
+                constant =  ScalarConstant(constant=c*u.AU)
+                observed = source * constant    
+                plt.plot(ph_energies_centers ,  observed(ph_energies), label='Const = '+str(c))
+            plt.loglog()
+            plt.legend()
+            plt.show()
+
+    """
     n_inputs = 1
     n_outputs = 1
 
