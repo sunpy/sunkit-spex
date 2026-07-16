1d02aa9cc6336871365684dc9da7eb5d2340dfe0
Iain Hannah <ianan@users.noreply.github.com>
2026-07-16 11:20:38 +0100

    further tidy up of plot params

--- sunkit_spex/legacy/fitting/fitter.py
+++ sunkit_spex/legacy/fitting/fitter.py
@@ -3545,20 +3545,27 @@ class Fitter:
                 pname = par_spec[0]
                 error = self.params["Error", p]
                 value = self.params["Value", p]
+                scl=1
+                # Might not have an error value so just change scale factor
+                # and apply this at the end when making the nice string
                 if pname.startswith("EM"):
-                    error = np.asarray(error) * 1e46
-                    value = value * 1e46
+                    scl=1e46
                 if pname.startswith("total_eflux"):
-                    error = np.asarray(error) * 1e35
-                    value = value * 1e35
+                    scl=1e35
+                if pname.startswith("plasma_d"):
+                    scl=1e10
                 value_fmt = ".2e"
                 error_fmt = ".2e"
-                if pname.startswith(("T", "index", "e_c", "C")):
+                # Only have f_vth, thick_fn and thick_warm as the models from photon_models_for_fitting.py
+                if pname.startswith(("T", "index", "e_c", "C", "loop_temp","length")):
                     value_fmt = ".2f"
                     error_fmt = ".2f"
                 if np.all(error) == np.all([0, 0]):
+                    value = value * scl
                     param_str += [pname + f": {value:{value_fmt}}" + "\n"]
                 else:
+                    value = np.asarray(value) * scl
+                    error = np.asarray(error) * scl
                     param_str += [pname + f": {value:{value_fmt}}"
                                 + f"$^{{+{error[1]:{error_fmt}}}}_{{-{error[0]:{error_fmt}}}}$"
                                 + "\n"
